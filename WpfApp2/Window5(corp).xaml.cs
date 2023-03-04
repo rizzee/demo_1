4690de8a0817a973b357ecbb0e03e70b9ed4dfad
marsh <marsh@2CHOV>
2023-03-04 12:25:17 +0700

    Добавьте файлы проекта.

---
+++ WpfApp2/Window5(corp).xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window5_corp_.xaml
+    /// </summary>
+    public partial class Window5_corp_ : Window
+    {
+        public Window5_corp_()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
